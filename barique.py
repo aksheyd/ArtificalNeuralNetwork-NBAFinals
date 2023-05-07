30c5145a07300ecab5b0bbde8f75c9a981a00852
Akshey D <aksheyd@umich.edu>
2023-05-06 17:49:30 -0700

    Update barique.py
    start analytics on warriors making finals?

--- barique.py
+++ barique.py
@@ -1,22 +1,32 @@
-#!/bin/env python
+#!/usr/bin/env python
 
-from nba_api.stats.endpoints import playercareerstats
+
+from nba_api.stats.endpoints import TeamYearByYearStats
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Nikola Jokić
-career = playercareerstats.PlayerCareerStats(player_id='203999') 
+# Golden State Warriors data
+data = TeamYearByYearStats(team_id=1610612744)
+tfdas = data.team_stats.get_data_frame()
+df = tfdas
 
+# only want Golden State era
+df.drop(df[(df['TEAM_CITY'] == "Philadelphia")].index, inplace=True)
+df.drop(df[(df['TEAM_CITY'] == "San Francisco")].index, inplace=True)
+
+
+print(df)
 # pandas data frames (optional: pip install pandas)
-df = career.get_data_frames()[0]
-
-print(list(df.columns.values))
-
-df['PLAYER_AGE'] = df['PLAYER_AGE'].astype(int)
+# df = career.get_data_frames()[0]
 
 
+# df['PLAYER_AGE'] = df['PLAYER_AGE'].astype(int)
 
-df.plot.scatter(y='TOV', x='PLAYER_AGE', figsize = (16,8))
 
-plt.show()
+
+# df.plot.scatter(y='TOV', x='PLAYER_AGE', figsize = (16,8))
+
+# plt.show()
+
+
